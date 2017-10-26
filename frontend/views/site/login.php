3a3cc89ddea267d4f7ce84fc7af884b09e0c3779
VOVANCHO <w-karpov@mail.ru>
2017-10-26 09:07:36 +0500

    0.4.2

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $LoginForm \domain\forms\LoginForm */
+/* @var $LoginForm \common\models\LoginForm */
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
