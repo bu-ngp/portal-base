b5b96b394b2dcee086b6baeeffde1b5535d80363
VOVANCHO <w-karpov@mail.ru>
2017-10-26 09:17:13 +0500

    0.4.2

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -8,7 +8,6 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
-use common\widgets\Alert;
 
 AppAsset::register($this);
 ?>
@@ -63,7 +62,6 @@ AppAsset::register($this);
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
-        <?= Alert::widget() ?>
         <?= $content ?>
     </div>
 </div>
