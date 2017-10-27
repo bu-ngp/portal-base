af9a38125759e0e028e92c5bc8744ebca69cc4fe
VOVANCHO <w-karpov@mail.ru>
2017-10-27 13:39:35 +0500

    0.4.2

--- backend/views/updates/index.php
+++ backend/views/updates/index.php
@@ -5,3 +5,14 @@
  * Date: 27.10.2017
  * Time: 13:10
  */
+
+use common\widgets\Documenter\Documenter;
+
+$this->title = Yii::t('common/updates','Updates');
+?>
+
+<div class="updates-index">
+    <?= Documenter::widget([
+
+    ]) ?>
+</div>
---
+++ common/widgets/Documenter/Documenter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sysadmin
+ * Date: 27.10.2017
+ * Time: 13:24
+ */
+
+namespace common\widgets\Documenter;
+
+
+use yii\base\Widget;
+
+class Documenter extends Widget
+{
+
+}
